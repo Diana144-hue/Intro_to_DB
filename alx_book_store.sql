b9485010b1dd4d61495da9195f911080b13f2730
unknown <dianachepngeno251@gmail.com>
2025-07-22 12:54:50 +0300

    Add schema for alx_book_store databese

---
+++ alx_book_store.sql
@@ -0,0 +1,37 @@
+Create DATABESE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+CREATE Authors Table
+CREATE TABLE Authors (
+     author_id INT AUTO_INCREMENT PRIMARY KEY,
+     author_name VARCHAR(215) NOT NULL
+);
+CREATE Books Table
+CREATE TABLE Books (
+     book_id INT AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(130) NOT NULL,
+     author_id INT,
+     price DOUBLE NOT NULL,
+     publication_date DATE,
+     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+Create Customers Table
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+Create Order_Dtails Table
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+)
+)
+)
+)
