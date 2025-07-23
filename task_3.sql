cc34e5d1526c4be51b2b29df7af9a6fb1657c474
Diana144-hue <dianachepngeno251@gmail.com>
2025-07-23 12:55:17 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,9 +1,10 @@
 -- task_3.sql
 -- USAGE:
---   mysql -u your_user -p alx_book_store < task_3.sql
+-- --   mysql -u your_user -p alx_book_store < task_3.sql
 
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_TYPE = 'BASE TABLE'
-ORDER BY TABLE_NAME;
+-- SELECT TABLE_NAME
+-- FROM INFORMATION_SCHEMA.TABLES
+-- WHERE TABLE_SCHEMA = DATABASE()
+--   AND TABLE_TYPE = 'BASE TABLE'
+-- ORDER BY TABLE_NAME;
+SHOW TABLES;
